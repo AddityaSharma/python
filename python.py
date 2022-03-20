7d3758d3e18e910d7f8b3c6d50b494ca1270221a
Adi_Anant <72218536+AddityaSharma@users.noreply.github.com>
2022-03-20 20:12:35 +0530

    Update python.py

--- python.py
+++ python.py
@@ -21,3 +21,45 @@ c = [4,5,6]
 d = c
 print(c == d) # true -> as both list cantains same elements.
 print(c is d) # true -> both the list share same memory address if we assign one list to another. *important*/
+
+# ----------------------------------------------------------------------------------------------------------------------------------------------------------------- #
+
+# lecture-7: Loops and iterations
+
+# for loop:
+nums = [1,2,3,4,5]
+
+# kind of for_each loop in c++
+for x in nums:
+    print(x)
+
+# range -> [0, 10)
+for i in range(10):  # single argument specifies the end point(exclusive)
+    print(i)
+
+# range -> [1, 11)
+for i in range(1, 11):  # first argument specifes the start point(inclusive), second argument the end point(exclusive)
+    print(i)
+
+
+# while loop:
+x = 0
+while x < 10:
+    print(x)
+    x += 1
+    
+# ----------------------------------------------------------------------------------------------------------------------------------------------------------------- #
+
+
+
+
+
+
+
+
+
+
+
+
+
+
